5fe4f38a29257d3078baf2ed91997db3c87ca065
juzisang <juziexe@gmail.com>
2019-06-05 17:46:15 +0800

    feat(app): 删除没用代码，添加muse ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import MuseUI from 'muse-ui'
+import 'typeface-roboto'
+import 'muse-ui/dist/muse-ui.css'
 
 Vue.config.productionTip = false
+
+Vue.use(MuseUI)
 
 new Vue({
   router,
